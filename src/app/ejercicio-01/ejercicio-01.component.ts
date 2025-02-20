31522489be8c930d9563630a5e38605255e3d419
Marcos <230733@utxicotepec.edu.mx>
2025-02-20 10:06:37 -0600

    Mejoras en el css

--- src/app/ejercicio-01/ejercicio-01.component.ts
+++ src/app/ejercicio-01/ejercicio-01.component.ts
@@ -2,16 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ejercicio-01',
-  imports: [],
   template: `
-    Hello Universe
+    <div class="hello-universe">
+      <h1>Hello Universe</h1>
+    </div>
   `,
-  styles: `
+  styles: [`
     :host {
       color: #a144eb;
-      
     }
-  `,
+
+    .hello-universe {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 10vh; /* Reduce la altura del div */
+      background-color:rgb(253, 236, 0);
+    }
+
+    h1 {
+      font-size: 4em;
+      text-align: center;
+    }
+  `]
 })
 export class Ejercicio01Component {
 
