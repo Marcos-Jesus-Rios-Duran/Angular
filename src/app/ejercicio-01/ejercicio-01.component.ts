f2ed1756bd4d57da127215c2a77892ed0fc4a0cc
Marcos <230733@utxicotepec.edu.mx>
2025-02-20 08:43:58 -0600

    Cargamos el proyecto

---
+++ src/app/ejercicio-01/ejercicio-01.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio-01',
+  imports: [],
+  template: `
+    Hello Universe
+  `,
+  styles: `
+    :host {
+      color: #a144eb;
+      
+    }
+  `,
+})
+export class Ejercicio01Component {
+
+}
---
+++ src/app/ejercicio-06/ejercicio-06.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio-06',
+  templateUrl: './ejercicio-06.component.html',
+  styleUrls: ['./ejercicio-06.component.css'] // Corrección aquí
+})
+export class Ejercicio06Component {
+  isEditable = true;
+}
